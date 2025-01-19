5c0557d1a3eca4345bcac20ec4fd8c2c56f495c4
kaushikhande <kaushikhande302@gmail.com>
2025-01-19 16:03:53 +0530

    Added es lint and called todo api.

--- src/App.js
+++ src/App.js
@@ -1,84 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-// import CommentIcon from '@mui/icons-material/Comment';
 
 function App() {
-  const [checked, setChecked] = React.useState([0]);
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [todos, setTodos] = useState([]);
+  axios.defaults.baseURL = 'http://localhost:3001/';
 
-  const handleToggle = (value: number) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
+  const handleToggle = (todo) => () => {
+    console.log(todo)
+  };
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
+  const fetchtodos = async () => {
+    const response = await axios.get('api/v1/todos',
+    {
+      headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3MzczNjQ3ODh9.cCmwh9Bu-XDITZbyqI2ZqHfu-TQ2XW1xo-JMNL5R2cA"}
+    } );
+    setTodos(response.data.todos);
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:3001/api/v1/todos', {headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3MzY4NzcyODR9.W5O4KyaxLBL6Xvazqg8TyBxBx-9S0IKVT-ay0yTJbpI"}} );
-        setData(response.data); // Axios automatically parses JSON
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
+    fetchtodos();
   }, []);
 
-    return (
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        {[0, 1, 2, 3].map((value) => {
-          const labelId = `checkbox-list-label-${value}`;
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {todos.map((todo) => {
+        const labelId = `checkbox-list-label-${todo.id}`;
 
-          return (
-            <ListItem
-              key={value}
-              secondaryAction={
-                <IconButton edge="end" aria-label="comments">
-                
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={checked.includes(value)}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ 'aria-labelledby': labelId }}
-                  />
-                </ListItemIcon>
-                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
-    );
+        return (
+          < ListItem key={todo.id} >
+            <ListItemButton onClick={handleToggle(todo)} >
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={false}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`${todo.title}`} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 }
 
 export default App;
